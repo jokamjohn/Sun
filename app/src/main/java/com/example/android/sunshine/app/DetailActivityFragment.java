2fb2fea6c9efac212165ba6a52cf4ac050ad3d72
johnkagga <johnkagga@gmail.com>
2015-10-30 03:46:39 +0300

    Added shareProvider and sharing implicit intents

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -3,18 +3,32 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-
+    //Tell the fragment that there is a menu
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
+
+    private final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private final String SHARE_HASH_TAG = "App created by John Kagga";
+
+    private String mForecastStr;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,12 +41,43 @@ public class DetailActivityFragment extends Fragment {
         //Checking for the intent and intent extra
         if (forecastIntent != null && forecastIntent.hasExtra(Intent.EXTRA_TEXT))
         {
-            String forecast = forecastIntent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = forecastIntent.getStringExtra(Intent.EXTRA_TEXT);
             //Adding forecast to the textview
             TextView detail = (TextView) rootView.findViewById(R.id.detail_textview);
-            detail.setText(forecast);
+            detail.setText(mForecastStr);
         }
 
         return rootView;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //Inflate the menu
+        inflater.inflate(R.menu.detail_fragment,menu);
+
+        //Locate the menu item
+        MenuItem item = menu.findItem(R.id.detail_menu_share);
+
+        //Fetch and store the shareActionProvider
+        ShareActionProvider mshareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mshareActionProvider != null)
+        {
+            mshareActionProvider.setShareIntent(createForecastShareIntent());
+        }
+        else
+        {
+            Log.d(LOG_TAG,"Share provider is null");
+        }
+    }
+
+   //Share intent
+    private Intent createForecastShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastStr + " " + SHARE_HASH_TAG);
+        return shareIntent;
+    }
+
 }
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +21,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -49,7 +43,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(settingsIntent);
             return true;
         }
+        else if (id == R.id.location_on_map)
+        {
+            showLocationOnMap();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showLocationOnMap() {
+        SharedPreferences locationPreference = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String location = locationPreference.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default_value));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location)
+                .build();
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,geoLocation);
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(mapIntent);
+        }
+        else
+        {
+            Log.d(LOG_TAG, "Could not call " + location);
+        }
     }
 }
