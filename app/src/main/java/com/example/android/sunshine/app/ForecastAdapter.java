14989183b168fe46e1d1098658eb41dbb9feaae4
johnkagga <johnkagga@gmail.com>
2015-11-16 00:55:12 +0300

    Choosing which view to use

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -14,6 +14,10 @@ import android.widget.TextView;
  * from a {@link Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -42,14 +46,39 @@ public class ForecastAdapter extends CursorAdapter {
                 " - " + highAndLow;
     }
 
+    //Which layout to show
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    //Number of layouts
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
     /*
-        Remember that these views are reused as needed.
+     Remember that these views are reused as needed.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        //Choose which view to show
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return view;
+        if (viewType == VIEW_TYPE_TODAY)
+        {
+            layoutId = R.layout.list_item_forecast_today;
+        }
+        else if (viewType == VIEW_TYPE_FUTURE_DAY)
+        {
+            layoutId = R.layout.list_item_forecast;
+        }
+
+       return LayoutInflater.from(context).inflate(layoutId, parent, false);
+
+
     }
 
     /*
